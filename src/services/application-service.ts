8e51e23016258fceaa6062ac225c898c5cb98546
Karthik Bharadwaj <kbharadwaj@veracode.com>
2024-08-08 11:24:15 +0530

    ENG-821 - debug

--- src/services/application-service.ts
+++ src/services/application-service.ts
@@ -157,6 +157,7 @@ export async function validateVeracodeApiCreds(inputs: Inputs): Promise<string |
     if (applicationResponse && applicationResponse?.api_credentials?.expiration_ts) {
       core.info(`VERACODE_API_ID and VERACODE_API_KEY is valid, Credentials expiration date - ${applicationResponse.api_credentials.expiration_ts}`);
     } else {
+      core.debug(`Validate API response - ${applicationResponse}`);
       core.setFailed('Invalid/Expired VERACODE_API_ID and VERACODE_API_KEY');
       annotations.push({
         path: '/',
