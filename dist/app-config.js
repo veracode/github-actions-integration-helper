5bed1c3add74e297a557fe18b30735f1e959cb99
Shailesh Hande <shande@veracode.com>
2025-10-06 15:12:11 +0530

    update code to handle artifact version based on the platformType paramter

---
+++ dist/app-config.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const appConfig = {
+    hostName: {
+        veracode: {
+            us: 'api.veracode.com',
+            eu: 'api.veracode.eu'
+        },
+        github: 'api.github.com'
+    },
+    api: {
+        veracode: {
+            applicationUri: '/appsec/v1/applications',
+            findingsUri: '/appsec/v2/applications',
+            sandboxUri: '/appsec/v1/applications/${appGuid}/sandboxes',
+            selfUserUri: '/api/authn/v2/users/self',
+            policyUri: '/appsec/v1/policies',
+            relayServiceUri: '/vrm-relay-service/api/scan-report',
+        },
+        github: ''
+    }
+};
+exports.default = appConfig;
